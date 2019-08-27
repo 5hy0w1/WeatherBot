5b535d89f37ee9b67f7d77e1ff98b95d761467ca
Artem <5hy0w1@mail.ru>
2019-08-27 13:10:42 +0300

    Убираем лишнее

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 API_REQUESTS_FREQUENCY = 3600  # 1 запрос в N секунд
 DUMP_FILE_PATH = 'users.bin'  # Путь к дамп-файлу
-TOKEN = "979571572:AAFzzshB1yM6C9elkMi3tBV71yxZ22Ckspo"  # Токен для телеграм бота
-WEATHER_TOKEN = "ac682c46-8018-40c8-b759-333c16c9ca1c"  # Токен к Яндекс.Погода
+TOKEN = "YOUR TOKEN"  # Токен для телеграм бота
+WEATHER_TOKEN = "YOUR TOKEN"  # Токен к Яндекс.Погода
